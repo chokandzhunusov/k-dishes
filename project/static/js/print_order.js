879eeb1d60c0060bdd53a17ca33b60574a4d118b
chokandzhunusov <chokan.dzhunusov@gmail.com>
2020-03-07 20:55:26 +0600

    Fix CSS at print page

---
+++ project/static/js/print_order.js
@@ -0,0 +1,9 @@
+$( "#remove-title-1-btn" ).on( "click", function() {
+    $("#print-title-div-1").css('display', 'none');
+    $("#remove-title-2-btn").css('display', 'none')
+})
+
+$( "#remove-title-2-btn" ).on( "click", function() {
+    $("#print-title-div-2").css('display', 'none');
+    $("#remove-title-1-btn").css('display', 'none')
+})
